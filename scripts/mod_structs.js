7018a2b3ea6cbc9e78ab6d32640bce75038448d0
jittosan <ratnajit.ss@gmail.com>
2021-12-07 17:00:55 +0800

    define basic modulecollection classes

---
+++ scripts/mod_structs.js
@@ -0,0 +1,123 @@
+class Database extends ModuleCollection {
+    constructor() {
+        super()
+    }
+
+    add_module(mod_info) {
+        return this.modules.#add(mod_info)
+    }
+
+    find_module(mod_info) {
+        return this.modules.#find(mod_info)
+    }
+
+    remove_module(mod_info) {
+        return this.modules.#remove(mod_info)
+    }
+}
+
+class Course extends ModuleCollection {
+    constructor(name='') {
+        super()
+        this.name = name
+    }
+
+    get_name() {
+        return this.name
+    }
+
+    set_name(name) {
+        this.name = name
+    }
+
+    add_module(mod_info) {
+        return this.modules.#add(mod_info)
+    }
+
+    find_module(mod_info) {
+        return this.modules.#find(mod_info)
+    }
+
+    remove_module(mod_info) {
+        return this.modules.#remove(mod_info)
+    }
+
+    compute_cap() {
+        return this.modules.compute_cap()
+    }
+
+    info() {
+        console.log(`COURSE: ${this.name}`)
+        this.modules.map((item) => {item.info()})
+    }
+}
+
+class Semester extends ModuleCollection {
+    constructor(num=0) {
+        super()
+        this.num = num
+    }
+
+    get_num() {
+        return this.num
+    }
+
+    set_num(num) {
+        this.num = num
+    }
+
+    add_module(mod_info) {
+        return this.modules.#add(mod_info)
+    }
+
+    find_module(mod_info) {
+        return this.modules.#find(mod_info)
+    }
+
+    remove_module(mod_info) {
+        return this.modules.#remove(mod_info)
+    }
+
+    compute_cap() {
+        return this.modules.compute_cap()
+    }
+
+    info() {
+        console.log(`SEMESTER: ${this.num}`)
+        this.modules.map((item) => {item.info()})
+    }
+}
+
+class AcademicRequirement extends ModuleCollection {
+    constructor(name='') {
+        super()
+        this.name = name
+    }
+
+    get_name() {
+        return this.name
+    }
+
+    set_name(name) {
+        this.name = name
+    }
+
+    add_module(mod_info) {
+        return this.modules.#add(mod_info)
+    }
+
+    find_module(mod_info) {
+        return this.modules.#find(mod_info)
+    }
+
+    remove_module(mod_info) {
+        return this.modules.#remove(mod_info)
+    }
+
+    info() {
+        console.log(`REQUIREMENT: ${this.name}`)
+        this.modules.map((item) => {item.info()})
+    }
+}
+
+//SCHEDULE CLASS
