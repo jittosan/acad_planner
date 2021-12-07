8c0317343062783c110afd5919bc4f9f8a36e40f
jittosan <ratnajit.ss@gmail.com>
2021-12-07 15:34:13 +0800

    complete linkedlist implementation

--- helper.js
+++ helper.js
@@ -38,8 +38,8 @@ class LinkedList {
 
     add(item){
         // add as root if list is empty
-        if (this.empty) {
-            this.root = Node(item)
+        if (this.empty()) {
+            this.root = new Node(item)
             this.root.set_next(this.root)
             this.root.set_prev(this.root)
             return true
@@ -49,8 +49,8 @@ class LinkedList {
                 return false
             }
             // add to end of list
-            new_node = Node(item)
-            last_node = this.root.get_prev()
+            let new_node = new Node(item)
+            let last_node = this.root.get_prev()
             last_node.set_next(new_node)
             new_node.set_prev(last_node)
             new_node.set_next(this.root)
@@ -69,8 +69,7 @@ class LinkedList {
             return this.root
         }
         // loop through rest of nodes
-        cur_node = this.root
-
+        let cur_node = this.root
         do {
             if (this.ident_fn(cur_node.get_item()) == identifier) {
                 if (raw_node) {return cur_node} else {return cur_node.get_item()}
@@ -85,33 +84,42 @@ class LinkedList {
 
     delete(identifier){
         // check identifier exists in list
-        del_node = this.find(identifier)
+        let del_node = this.find(identifier)
         if (del_node == null) {return false}
         // check if del_node is the last element
-        if (this.size() == 1) {
+        if (this.single()) {
             this.root = null
             return true
         }
         // remove del_node
-        prev_node = del_node.get_prev()
-        next_node = del_node.get_next()
+        let prev_node = del_node.get_prev()
+        let next_node = del_node.get_next()
         prev_node.set_next(next_node)
         next_node.set_prev(prev_node)
+        // update root if del_node is the root
+        if (del_node == this.root) {
+            this.root = del_node.get_next()
+        }
         return true
     }
 
-    empty() {
-        return this.root == null;
+
+    empty(){
+        return (this.root == null)
     }
 
-    size() {
+    single() {
+        return (this.root.get_next() == this.root)
+    }
+
+    size(){
         // check empty
         if (this.empty()) {
             return 0
         } else {
             // iterate through all nodes
-            count = 0
-            cur_node = this.root
+            let count = 0
+            let cur_node = this.root
             do {
                 count += 1
                 cur_node = cur_node.get_next()
@@ -120,9 +128,35 @@ class LinkedList {
         }
     }
 
-    map() {
-        return 1;
+    map(fn) {
+        let output = []
+        //check list empty
+        if (this.empty()) {
+            return output
+        }
+        //map through nodes and store output
+        let cur_node = this.root
+        do {
+            output += [fn(cur_node.get_item())]
+            cur_node = cur_node.get_next()
+        } while (cur_node != this.root)
+        return output
     }
 }
 
-let a = new LinkedList((item) => {item})
+// let a = new LinkedList((item) => {return item})
+// a.add(1)
+// console.log(a.size())
+// a.add(3)
+// console.log(a.size())
+// a.delete(1)
+// console.log(a.size())
+// console.log('find 1')
+// console.log(a.find(1))
+// console.log('find 3')
+// console.log(a.find(3))
+// console.log('test')
+// a.add(4)
+// a.add(7)
+// a.add(97)
+// a.map((item) => {console.log(item)})
