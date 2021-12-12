4ada7fa5737b24191550a3162e98c02e65151466
jittosan <ratnajit.ss@gmail.com>
2021-12-12 21:40:58 +0800

    style acad req dropdown

--- frontend/src/components/AcademicRequirements.js
+++ frontend/src/components/AcademicRequirements.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { BsCheckCircle, BsDashCircle } from 'react-icons/bs'
 import {RiArrowDownSLine,RiArrowRightSLine} from 'react-icons/ri'
 import styles from './AcademicRequirements.module.scss'
 import Tag from './Tag'
@@ -17,11 +18,27 @@ const RequirementGroup = ({req_data}) => {
     const toggleSelect = () => {setSelected(!selected)}
 
     return (
+        <>
         <div className={styles.requirementGroup} onClick={toggleSelect} >
             {selected ? <RiArrowRightSLine className={styles.dropdownIcon} /> : <RiArrowDownSLine className={styles.dropdownIcon} />}
             <Tag code={req_data.name.substring(0,3)} color={'#ec3cec'}/>
-            <p>{req_data.name}</p>
-            {selected ? req_data.modules.map((item) => <p>{item}</p>) : ''}
+            <p className={styles.titleText}>{req_data.name}</p>
+        </div>
+        {selected ? 
+                <div className={styles.dropdownContainer}>
+                    {req_data.modules.map((item) => <RequiredModule code={item} />)}
+                </div>
+            : ''}
+        </>
+    )
+}
+
+const RequiredModule = ({code}) => {
+    const [completed, setCompleted] = useState(false)
+    return(
+        <div className={styles.requiredModule} onClick={() => setCompleted(!completed)}>
+            {completed ? <BsCheckCircle className={styles.checkIcon} /> : <BsDashCircle className={styles.dashIcon} />}
+            <p>{code}</p>
         </div>
     )
 }
