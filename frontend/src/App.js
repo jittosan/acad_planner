be351226e0d63f593dd67d83a9d4d4713ecc74ce
jittosan <ratnajit.ss@gmail.com>
2021-12-23 23:13:52 +0800

    touch up overall style and schedule selection logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import SemesterContainer from './components/SemesterContainer';
 import AcademicRequirements from './components/AcademicRequirements';
-import ScheduleHeader from './components/ScheduleHeader';
 import ScheduleContainer from './components/ScheduleContainer';
 import { ScheduleContextProvider } from './context/ScheduleContext';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import scheduleData from './data/schedule.json'
 
 //define test data
 let acad_req_data = [
@@ -22,82 +22,20 @@ let acad_req_data = [
   },
 
 ]
-let sem_data = [
-  {
-      name:'Y1S1',
-      acad_year: 'AY20/21',
-      type:'SEM 1',
-      id: 1,
-      modules:['ESP1111', 'MA1512', 'MA1511']
-  },
-  {
-      name:'Y1S2',
-      acad_year: 'AY20/21',
-      type:'SEM 2',
-      id: 1,
-      modules:['CS1010E', 'ESP2111']
-  },
-  {
-      name:'Y2S1',
-      acad_year: 'AY21/22',
-      type:'SEM 1',
-      id: 1,
-      modules:['PC2031', 'ME2121', 'GESS1014']
-  },
-  {
-      name:'Y2S2',
-      acad_year: 'AY21/22',
-      type:'SEM 2',
-      id: 1,
-      modules:['GEC1010', 'ESP5402', 'MA1511']
-  },
-  {
-      name:'Y2 Summer',
-      acad_year: 'AY22/23',
-      type:'ST I',
-      id: 1,
-      modules:['PC2020', 'ESP2111']
-  },
-  {
-      name:'Y3S1',
-      acad_year: 'AY22/23',
-      type:'SEM 1',
-      id: 1,
-      modules:['MA1508E', 'ME2121', 'GEA1000']
-  },
-] 
 
-let scheduleData = [
-  {
-    name:'First',
-    semesters: [...sem_data]
-  },
-  {
-    name:'Alternate',
-    semesters: [...sem_data]
-  },
-  {
-    name:'Extreme',
-    semesters: [...sem_data]
-  },
-]
-// const schedule_title = 'Ultimate Combi'
-console.log(scheduleData)
 function App() {
   const [currentSchedule, setCurrentSchedule] = useState(0)
-
-  const [schedule, setSchedule] = useState(scheduleData[currentSchedule]['semesters'])
-  console.log(schedule)
+  const selectSchedule = (index) => {console.log(index);setCurrentSchedule(index);trigger()}
+  let schedule = scheduleData[currentSchedule]['semesters']
   const [flip, setFlip] = useState(true)
   const trigger = () => (setFlip(!flip))
-  const update = (newState) => {setSchedule(newState);trigger()}
+  const update = (newState) => {trigger()}
 
   return (
     <div className="App">
-      {/* <ScheduleHeader title={'Academic Planner'}/> */}
       <ScheduleContextProvider value={schedule} update={update}>
       <div className='MainContainer'>
-        <ScheduleContainer acad_req_data={acad_req_data} schedule={scheduleData} update={setCurrentSchedule} current={currentSchedule}/>
+        <ScheduleContainer schedule={scheduleData} update={selectSchedule} current={currentSchedule}/>
         <SemesterContainer />
         <AcademicRequirements acad_req_data={acad_req_data} />
       </div>
--- frontend/src/components/ScheduleContainer.js
+++ frontend/src/components/ScheduleContainer.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react'
 import { HiOutlineClipboardList } from 'react-icons/hi'
+import { MdSaveAlt, MdSettings } from 'react-icons/md'
 import styles from '../styles/ScheduleContainer.module.scss'
 
 const ScheduleContainer = ({acad_req_data, schedule, update, current}) => {
     return (
         <div className={styles.container}>
-            <strong>Schedules</strong>
+        <div className={styles.titleCard}>
+            <h2>Academic Planner</h2>
+        </div>
+        <br />
+
+        <div>
+        <strong>Schedules</strong>
             {schedule.map((item, index) => <ScheduleItem key={index} title={item.name} update={() => update(index)} selected={current===index} />)}
+        <p>.</p>
+        </div>
+
+        <br />
+        <div className={styles.actionTray}>
+            <MdSaveAlt className={styles.icon} />
+            <MdSettings className={styles.icon} />
+        </div>
         </div>
     )
 }
