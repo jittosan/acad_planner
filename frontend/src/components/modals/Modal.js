d657488db91a031af437ae15e7d0dd3f6ad79883
jittosan <ratnajit.ss@gmail.com>
2021-12-24 15:54:11 +0800

    define modal component

--- frontend/src/components/modals/Modal.js
+++ frontend/src/components/modals/Modal.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import styles from '../../styles/Modal.module.scss'
 
-const Modal = () => {
+//  <Modal title={'Confirm Delete'} text={'Are you sure you want to delete module from semester?'} options={[{name:'Confirm',action:()=>console.log('confirm modal')},{name:'Cancel',action:()=>console.log('cancel modal'), highlight:true}]} toggle={() => console.log('close')} />
+
+
+const Modal = ({title, options, text, toggle}) => {
     return (
-        <div>
-            <p>Modals</p>
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <strong>{title}</strong>
+                <p>{text}</p>
+                <div className={styles.optionContainer}>
+                    {options.map((item, index) => <ModalOption key={index} text={item.name} action={() => {item.action();toggle()}} highlight={item.highlight}/>)}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ModalOption = ({text, action, highlight}) => {
+    return (
+        <div className={`${styles.option} ${highlight ? styles.highlight : ''}`} onClick={action}>
+            <p>{text}</p>
         </div>
     )
 }
